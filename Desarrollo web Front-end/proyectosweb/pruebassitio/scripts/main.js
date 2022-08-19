bb6b017fb11f0aab6516539e24e1152aaf442f72
miguel-554 <romero.tapia.m.angel@gmail.com>
2022-08-19 18:52:31 -0500

    Nuevos cambios

---
+++ Desarrollo web Front-end/proyectosweb/pruebassitio/scripts/main.js
@@ -0,0 +1,27 @@
+let miImage = document.querySelector('img');
+miImage.onclick = function() {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/crash.jpg') {
+        miImage.setAttribute('src', 'images/crash.jpg');
+    } else {
+        miImage.setAttribute('src', 'images/crash.jpg');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector('h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if (!miNombre) {
+        estableceNombreUsuario();
+    } else {
+        localStorage.setItem('nombre', miNombre);
+        miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+    }
+}
+
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
